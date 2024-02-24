15fc0b4b17bcfee07145d5f7a4551f94d6e0da02
Mohitxroxx <mohitsinghroxx@gmail.com>
2024-02-24 18:41:11 +0530

    added info routes for attractions,lifeonland and endangered species

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,12 +7,20 @@ const user=require('../controller/data')
 const register=require('../controller/data') 
 const upload=require('../controller/data') 
 const post=require('../controller/data') 
+const attractions=require('../controller/data') 
+const lifeonland=require('../controller/data') 
+const endangered=require('../controller/data') 
+const ask=require('../controller/data') 
 const getpost=require('../controller/data') 
 
 
 router.post('/register',register)
 router.post('/upload',upload)
 router.post('/post',post)
+router.post('/attractions',attractions)
+router.post('/lifeonland',lifeonland)
+router.post('/endangered',endangered)
+router.post('/ask',ask)
 router.post('/getpost',getpost)
 
 
